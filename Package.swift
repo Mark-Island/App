9167af4ce6ce6f8a6b198c2eff44abec558578f5
marcprux <mwp1@cornell.edu>
2021-08-11 20:23:47 -0400

    chore: checkpoint

--- Package.swift
+++ Package.swift
@@ -1,53 +1,44 @@
 // swift-tools-version:5.5
-/**
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 import PackageDescription
 
 let package = Package(
-    name: "App",
-    platforms: [
-        .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
-        //.macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
-    ],
-    products: [
-        .library(name: "App", targets: ["App"])
-    ],
+    name: "Mark-Island",
+    platforms: [ .macOS(.v11), .iOS(.v14) ],
+    products: [ .library(name: "App", targets: ["App"]) ],
     dependencies: [
-        .package(url: "https://github.com/appfair/Fair.git", .branch("main"))
-    ]
-    + externalDependencies().map(\.package),
+        .package(name: "Fair", url: "https://github.com/appfair/Fair.git", .branch("main")),
+    ],
     targets: [
-        .target(name: "App", dependencies: [
-            .product(name: "FairApp", package: "Fair")
-        ] + externalDependencies().flatMap(\.targets),
-            resources: [.copy("Bundle")]),
-        .testTarget(name: "AppTests", dependencies: [
-            "App"
-        ]),
+        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair") ], resources: [.copy("Bundle")]),
+        .testTarget(name: "AppTests", dependencies: [.product(name: "FairApp", package: "Fair")]),
     ]
 )
 
-// Everything above this line must remain unmodified.
+// The following validations are required in order for the package to be integrated by the App Fair
 
-/// Return an array of tuples for each of the app's dependencies: `(Package.Dependency, [Target.Dependency])`
-func externalDependencies() -> [(package: PackageDescription.Package.Dependency, targets: [PackageDescription.Target.Dependency])] {
-    [
-        //(.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.0")), [.product(name: "Logging", package: "swift-log")]),
-        //(.package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")), [.product(name: "Crypto", package: "swift-crypto")]),
-        //(.package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.1.0")), [.product(name: "CoreMetrics", package: "swift-metrics"), .product(name: "Metrics", package: "swift-metrics")]),
-    ]
-}
+precondition(!package.dependencies.isEmpty, "package must have at least one dependency")
+precondition(package.dependencies[0].name == "Fair", "first dependency name must be \"Fair\"")
+precondition(package.dependencies[0].url == "https://github.com/appfair/Fair.git" || package.dependencies[0].url == "git@github.com:appfair/Fair.git", "first package dependency must be https://github.com/appfair/Fair.git")
+
+precondition(package.products.count == 1, "package must have exactly one product")
+precondition(package.products[0].name == "App", "package product must be named \"App\"")
+
+// validate target names and source paths
+
+precondition(package.targets.count == 2, "package must have exactly two targets named \"App\" and \"AppTests\"")
+precondition(package.targets[0].name == "App", "first target must be named \"App\"")
+precondition(package.targets[0].path == nil || package.targets[0].path == "Sources", "first target path must be named \"Sources\"")
+precondition(package.targets[0].sources == nil, "first target sources must be empty")
+
+precondition(package.targets[1].name == "AppTests", "second target must be named \"AppTests\"")
+precondition(package.targets[1].path == nil || package.targets[1].path == "Tests", "second target must be named \"Tests\"")
+precondition(package.targets[1].sources == nil, "second target sources must be empty")
+
+precondition(!package.targets[0].dependencies.isEmpty, "package target must have at least one dependency")
+
+// Target.Depencency is opaque and non-equatable, so resort to using the description for validation
+precondition(String(describing: package.targets[0].dependencies[0]) == "productItem(name: \"FairApp\", package: Optional(\"Fair\"), condition: nil)", "for package dependency must be FairApp")
+precondition(String(describing: package.platforms?.first) == "Optional(PackageDescription.SupportedPlatform(platform: PackageDescription.Platform(name: \"macos\"), version: Optional(\"11.0\")))", "package must support macOS 11")
+precondition(String(describing: package.platforms?.last) == "Optional(PackageDescription.SupportedPlatform(platform: PackageDescription.Platform(name: \"ios\"), version: Optional(\"14.0\")))", "package must support iOS 14")
 
 
